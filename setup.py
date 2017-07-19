b26d92209643c2378e390bf080e756a1a4db105a
ogierpaul <ogier.paul@gmail.com>
2017-07-19 16:02:55 +0200

    reformat code

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
 from distutils.core import setup
 
 setup(
-    name = 'neat-martinet',
-    packages = ['neat-martinet'],
-    version = '0.1',
-    description = 'Simple table and string cleaning functions implemented in unreadable Python.',
-    author = 'Amber Ocelot',
-    author_email = 'ogier.paul@gmail.com',
-    copyright = 'na',
-    url = 'https://github.com/ogierpaul/neat-martinet',
-    download_url = 'https://github.com/ogierpaul/neat-martinet.git',
-    keywords = ['pandas', 'data cleaning'],
-    classifiers = [
+    name='neat-martinet',
+    packages=['neat-martinet'],
+    version='0.1',
+    description='Simple table and string cleaning functions implemented in unreadable Python.',
+    author='Amber Ocelot',
+    author_email='ogier.paul@gmail.com',
+    url='https://github.com/ogierpaul/neat-martinet',
+    download_url='https://github.com/ogierpaul/neat-martinet.git',
+    keywords=['pandas', 'data cleaning'],
+    classifiers=[
         'Programming Language :: Python :: 3',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: End Users/Desktop',
-        'Operating System :: MacOS',
-        'Operating System :: Unix',
         'Topic :: Utilities'
-        ]
+    ]
 )
